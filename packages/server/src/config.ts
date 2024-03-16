5fcf27923094d6a577fa1d4dc5cdc347066bb17c
Jeremy Colin <jeremypa.colin@gmail.com>
2024-03-16 21:37:43 +0100

    trying without host config

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -7,5 +7,4 @@ const isDev = process.env.NODE_ENV !== "production";
 export const config = {
   DATABASE_CONNECTION_STRING: process.env.DATABASE_PRIVATE_URL,
   PORT: isDev ? 8080 : parseInt(process.env.PORT!, 10),
-  HOST: isDev ? "localhost" : "0.0.0.0",
 };
