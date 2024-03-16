ff6b17ab031f5b21f70e50021faa853bbfbf4556
Jeremy Colin <jeremypa.colin@gmail.com>
2024-03-16 20:55:41 +0100

    debug railway db

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -7,15 +7,21 @@ import pastifyPostgres from "@fastify/postgres";
 export const setupDatabase = fastifyPlugin(async (fastify, options) => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  await fastify.register(pastifyPostgres, {
-    connectionString: process.env.DATABASE_PRIVATE_URL,
-  });
+  console.log(process.env.DATABASE_PRIVATE_UR);
 
-  await fastify.pg.query(`
+  try {
+    await fastify.register(pastifyPostgres, {
+      connectionString: process.env.DATABASE_PRIVATE_URL,
+    });
+
+    await fastify.pg.query(`
     CREATE TABLE IF NOT EXISTS treasure_hunts (
     id SERIAL NOT NULL PRIMARY KEY,
     created_by VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
     treasure_hunt JSON NOT NULL);
   `);
+  } catch (error) {
+    console.error("Unable to setup database", error);
+  }
 });
