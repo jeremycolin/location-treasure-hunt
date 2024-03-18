c33b4653ef40787eec64bd042f18d4aa179318a2
Jeremy Colin <jeremypa.colin@gmail.com>
2024-03-18 23:29:35 +0100

    deploy next api rewrite

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const API_URL = process.env.API_URL || "http://localhost:8080";
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/api/:path",
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
