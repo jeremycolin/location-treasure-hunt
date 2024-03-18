5a6859679f126125cd69122de38b61f281a697dd
Jeremy Colin <jeremypa.colin@gmail.com>
2024-03-19 00:32:30 +0100

    Fastify ignore trailing slash

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,7 +5,7 @@ import { setupDatabase } from "./database";
 import { treasureHuntsRoutes } from "./treasure-hunts";
 
 async function startServer() {
-  const server = fastify({ logger: true });
+  const server = fastify({ logger: true, ignoreTrailingSlash: true });
   server.register(helmet);
   await server.register(setupDatabase);
   server.register(treasureHuntsRoutes);
